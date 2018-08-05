603c9e751a28d9949fb0d31dd9f8dfe571c0d65c
Alex Belyeu <abelyeun@gmail.com>
2018-08-05 16:53:47 -0400

    Add API key to Google Map

--- src/components/Map.js
+++ src/components/Map.js
@@ -9,8 +9,9 @@ import {
 
 const Map = compose(
   withProps({
-    googleMapURL:
-      'https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places',
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${
+      process.env.REACT_APP_GOOGLE_MAPS_API_KEY
+    }&v=3.exp&libraries=geometry,drawing,places`,
     loadingElement: <div style={{ height: `100px` }} />,
     containerElement: <div style={{ height: `400px` }} />,
     mapElement: <div style={{ height: `100%` }} />,
